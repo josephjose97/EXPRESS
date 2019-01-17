b0ff264e782ffd88b685a90e21ba65ece17498ef
josephjose97 <46046449+josephjose97@users.noreply.github.com>
2019-01-17 22:57:36 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var us=require('../database/user')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register.html', { title: 'Express' });
+});
+router.get('/next', function(req, res, next) {
+  res.render('next.html', { title: 'Express' });
+});
+router.post('/add',(req, res)=> {
+  var myData = new us(req.body);
+  myData.save()
+  .then(item =>{
+    res.send("item saved to database succssfully");
+  })
+  .catch(err => {
+    res.status(400).send("unable to save to database");
+  })
+  
+});
+router.get('/get',function(req,res,next){
+  us.find()
+  .then(function(doc){
+    res.render('view',{item:doc})
+  });
+});
+router.get('/get',function(req,res,next){
+  us.find()
+  .then(function(doc){
+    res.render('view',{item:doc})
+  });
+});
+module.exports = router;
